b01e86922abd21461d54dc3e1d0ed6db3599be50
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-02-02 10:40:44 +0200

    feat : background image has been set

--- styles/Styled.Global.js
+++ styles/Styled.Global.js
@@ -1,16 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
+import backgroundImage from "../public/images/background.png";
+
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap');
-  * {
+   * {
     box-sizing: border-box;
-  }
+    }
   body {
-    background: ${({ theme }) => theme.colors.body};
+    background-image: url(${backgroundImage});
     color: hsl(192, 100%, 9%);
     font-family: 'Poppins', sans-serif;
     font-size: 1.15em;
     margin: 0;
+    background-size: cover;
   }
   p {
     opacity: 0.6;
