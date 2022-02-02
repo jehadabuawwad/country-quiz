18376d65cae4562abf5cc3503a7f6f69f40b1eda
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-02-02 10:34:33 +0200

    build : next-images dependency added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+const withImages = require("next-images");
+module.exports = withImages({
+  images: {
+    disableStaticImages: true,
+  },
   reactStrictMode: true,
-}
+  fileExtensions: ["jpg", "jpeg", "png", "gif", "svg"],
+  webpack(config, options) {
+    return config;
+  },
+});
